398a43c6701f1c7cba4bcd04c28f18dcf2e3bc8b
luaschelb <lua.schelb@gmail.com>
2025-10-04 13:39:22 -0300

    Implemented working delete route and scaffolded update route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import turso from "./db";
+require('dotenv').config()
 
 const app = fastify();
 
@@ -12,13 +13,31 @@ app.get("/", async (request, reply) => {
 
 app.get("/items", async (request, reply) => {
     const itens = await turso.execute("SELECT * FROM items");
-    if(!itens)
+    if(!itens.rows.length)
+    {
+        reply.status(404).send({message: "No Items found"})
+    }
+    reply.send(itens.rows)
+})
+
+app.put("/items/:id", async (request, reply) => {
+    const itens = await turso.execute("SELECT * FROM items");
+    if(!itens.rowsAffected)
     {
         reply.status(404).send({message: "No Item found"})
     }
     reply.send(itens.rows)
 })
 
-app.listen({ port: 3333 }, () => {
-    console.log("Server is running on port 3333");
+app.delete("/items/:id", async (request, reply) => {
+    const params = request.params as { id: number }
+    const itens = await turso.execute("DELETE FROM items WHERE items.id = ?", [params.id]);
+    if(!itens.rowsAffected)
+    {
+        reply.status(404).send({message: "No Item found"})
+    }
+    reply.send()
+})
+app.listen({ port: Number(process.env.PORT)}, () => {
+    console.log("Server is running on port " + Number(process.env.PORT));
 });
