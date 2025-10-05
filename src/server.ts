4212d2d3df13a0698329e8078a528bea2070d03b
luaschelb <lua.schelb@gmail.com>
2025-10-05 15:45:12 -0300

    Made changes to tsconfig and added try catch to routes

--- src/server.ts
+++ src/server.ts
@@ -8,36 +8,53 @@ const app = fastify();
 app.register(fastifyCors, { origin: "*" });
 
 app.get("/", async (request, reply) => {
-    return reply.status(200).send({ message: "Hello World" });
+    try {
+        return reply.status(200).send({ message: "Hello World" });
+    } catch (error) {
+        reply.status(500).send({ message: "Internal Server Error" });
+    }
 });
 
 app.get("/items", async (request, reply) => {
-    const itens = await turso.execute("SELECT * FROM items");
-    if(!itens.rows.length)
-    {
-        reply.status(404).send({message: "No Items found"})
+    try {
+        const itens = await turso.execute("SELECT * FROM items");
+        if(!itens.rows.length)
+        {
+            return reply.status(404).send({message: "No Items found"})
+        }
+        reply.send(itens.rows)
+    } catch (error) {
+        reply.status(500).send({ message: "Internal Server Error" });
     }
-    reply.send(itens.rows)
-})
+});
 
 app.put("/items/:id", async (request, reply) => {
-    const itens = await turso.execute("SELECT * FROM items");
-    if(!itens.rowsAffected)
-    {
-        reply.status(404).send({message: "No Item found"})
+    try {
+        const itens = await turso.execute("SELECT * FROM items");
+        if(!itens.rowsAffected)
+        {
+            return reply.status(404).send({message: "No Item found"})
+        }
+        reply.send(itens.rows)
+    } catch (error) {
+        reply.status(500).send({ message: "Internal Server Error" });
     }
-    reply.send(itens.rows)
-})
+});
 
 app.delete("/items/:id", async (request, reply) => {
-    const params = request.params as { id: number }
-    const itens = await turso.execute("DELETE FROM items WHERE items.id = ?", [params.id]);
-    if(!itens.rowsAffected)
-    {
-        reply.status(404).send({message: "No Item found"})
+    try {
+        const params = request.params as { id: number }
+        const itens = await turso.execute("DELETE FROM items WHERE items.id = ?", [params.id]);
+        if(!itens.rowsAffected)
+        {
+            return reply.status(404).send({message: "No Item found"})
+        }
+        reply.send()
+    } catch (error) {
+        reply.status(500).send({ message: "Internal Server Error" });
     }
-    reply.send()
 })
+
 app.listen({ port: Number(process.env.PORT)}, () => {
     console.log("Server is running on port " + Number(process.env.PORT));
 });
